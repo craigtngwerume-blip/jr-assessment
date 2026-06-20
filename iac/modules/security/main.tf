4a44c4e49dda29ac5c41b745586cbb0fb52ff104
Craig <craig@jr-assessment.local>
2026-06-20 21:51:33 +0200

    fixed vpc and ami

--- iac/modules/security/main.tf
+++ iac/modules/security/main.tf
@@ -9,7 +9,7 @@ resource "aws_security_group" "web" {
   vpc_id      = var.vpc_id
 
   ingress {
-    description = "SSH from my IP only — least privilege"
+    description = "SSH from my IP only least privilege"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
